5f03e5e6a9f82776aab24934d1418ec3b1c467fd
Wenhan Ding <wding@f0r6s10.clusters.42paris.fr>
2025-12-16 18:14:17 +0100

    robotomy fini

---
+++ cpp05/ex02/RobotomyRequestForm.cpp
@@ -0,0 +1,33 @@
+#include "RobotomyRequestForm.hpp"
+
+RobotomyRequestForm::RobotomyRequestForm(const std::string &target) : AForm("Robotomy Request", 72, 45, target)
+{
+}
+
+RobotomyRequestForm::RobotomyRequestForm(const RobotomyRequestForm &other) : AForm(other)
+{
+}
+
+RobotomyRequestForm &RobotomyRequestForm::operator=(const RobotomyRequestForm &other)
+{
+	AForm::operator=(other);
+	return *this;
+}
+
+RobotomyRequestForm::~RobotomyRequestForm()
+{
+}
+
+void RobotomyRequestForm::action() const
+{
+	std::cout << "* VRRRRRRR... DDDDDDzzz... drilling noises... *" << std::endl;
+
+	if (std::rand() % 2)
+	{
+		PRINT(this->getTarget() << " has been successfully robotomized!");
+	}
+	else
+	{
+		PRINT("Robotomy failed for " << this->getTarget() << ".");
+	}
+}
